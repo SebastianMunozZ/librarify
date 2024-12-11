7a3782110517bf6fda87f272bae57da20d3511e9
Sebastian Muñoz <samunoz@rayensalud.com>
2024-12-11 11:51:44 -0300

    se suben cambios

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;    
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
     #[Route('/library/list', name: 'library_list')]
-    public function list()
+    public function list(Request $request)
     {
-        $response = new Response();
-        $response->setContent('<div>Hola mundo </div>');
+        $title = $request->query->get('title');
+        $response = new JsonResponse();
+        $response->setData([
+            "success" => true,
+            "data" => [
+                [
+                    'id' => 1,
+                    'title' => "Libro1"
+                ],    
+                [
+                    'id' => 2,
+                    'title' => "Libro2"
+                ],
+                [
+                    'id' => 3,
+                    'title' => $title
+                ]
+            ]
+        ]);
         
         return $response;
 
